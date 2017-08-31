1fbba720904e15d9d9166dfbcccb0ab984175a53
FilippovAM <andrey.filippov@anychart.com>
2017-08-31 12:40:38 +0800

    fix

---
+++ demos/Simple_Map/node_modules1/anychart-angular2/demos/Simple_Charts_On_Stage/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { AnychartService } from 'anychart-angular2/components';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  template: `<div anychartStage style="width: 800px; height: 400px"></div>`,
+})
+export class AppComponent {
+
+  constructor(private acService: AnychartService) {
+    let pie = anychart.pie(this.myData);
+    pie.title('Top 5 pancake fillings');
+    pie.bounds(0, 0, '50%', '100%');
+
+
+    let line = anychart.line(this.myData);
+    line.title('Top 5 pancake fillings');
+    line.bounds('50%', 0, '50%', '100%');
+
+    this.acService.addChart(pie);
+    this.acService.addChart(line, function(chart) {
+      chart.title('After Draw Title');
+    });
+  }
+
+  myData = [
+    ["Chocolate", 5],
+    ["Rhubarb compote", 2],
+    ["Crêpe Suzette", 2],
+    ["American blueberry", 2],
+    ["Buttermilk", 1]
+  ];
+
+
+  ngAfterViewInit() {
+    this.acService.drawAll();
+  }
+}

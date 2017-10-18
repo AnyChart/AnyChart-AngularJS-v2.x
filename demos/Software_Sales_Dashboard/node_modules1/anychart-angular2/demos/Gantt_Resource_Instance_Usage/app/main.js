9adad86bfb00023b99a0beab286b6a050634a926
anychart.com production <www-data@www.anychart.com>
2017-10-18 04:02:43 -0400

    fix

--- demos/Software_Sales_Dashboard/node_modules1/anychart-angular2/demos/Gantt_Resource_Instance_Usage/app/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-var components_1 = require('anychart-angular2/components');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule, [components_1.AnychartService]);
-//# sourceMappingURL=main.js.map
